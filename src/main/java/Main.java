1c88529c4aa5dc7b546eb5b8f2d5be87b406eee5
AndreiChernyshev <andreiorsk79@gmail.com>
2023-12-11 16:00:39 +0500

    Step 2. ForkJoin features added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,18 +2,26 @@ import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.concurrent.ForkJoinPool;
 
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        String folderPath = "C://skillbox";
+        String folderPath = "C:\\Spring";
         File file = new File(folderPath);
         long timeStamp = System.currentTimeMillis();
         System.out.println(getFolderSize(file));
         System.out.println(System.currentTimeMillis() - timeStamp);
         //nio2
-        long size = Files.walk(Path.of("C:\\skillbox\\FilesProject")).mapToLong(p -> p.toFile().length()).sum();
-        System.out.println(size);
+//        long size = Files.walk(Path.of("C:\\skillbox\\FilesProject")).mapToLong(p -> p.toFile().length()).sum();
+//        System.out.println(size);
+        //forkJoin
+        FolderSizeCalculator calculator = new FolderSizeCalculator(file);
+        ForkJoinPool pool = new ForkJoinPool();
+        timeStamp = System.currentTimeMillis();
+        System.out.println(pool.invoke(calculator));
+        System.out.println(System.currentTimeMillis() - timeStamp);
+
     }
 
     public static long getFolderSize(File folder) {
